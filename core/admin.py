c22540a9590c714d5e727f11ed0b461bb09a1c72
VadimGRSM <vadimgrsm8923@gmail.com>
2025-06-14 20:48:15 +0300

    Created several models (Drawing, Order, Material, Detail), and slightly configured the admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Drawing, Order, Material, Detail
 
-# Register your models here.
+
+@admin.register(Drawing)
+class DrawingAdmin(admin.ModelAdmin):
+    list_display = ['user', 'slug', 'file_name', 'file_path', 'original_filename', 'file_size',
+                    'uploaded_at', 'description']
+    prepopulated_fields = {'slug': ('file_name',)}
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'order_number', 'status', 'total_cost', 'created_at', 'updated_at', 'notes']
+    list_filter = ['order_number', 'status']
+    list_editable = ['status']
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ['material_name', 'material_type', 'price_unit_area', 'properties', 'available']
+    list_filter = ['available', 'material_type']
+    list_editable = ['available']
+
+
+@admin.register(Detail)
+class DetailAdmin(admin.ModelAdmin):
+    list_display = ['drawing', 'order', 'material', 'length', 'width', 'thickness', 'quantity',
+                    'calculated_cost', 'created_at', 'updated_at']
+    list_filter = ['material']
